522e5679dd23c3ede9ebc9b2f70e70044c1e8c1f
jiyeong1004 <ak1157@naver.com>
2020-11-19 19:38:12 +0900

    Complete ImageDialog

--- app/src/main/java/com/example/mirimdialogtest3/MainActivity.java
+++ app/src/main/java/com/example/mirimdialogtest3/MainActivity.java
@@ -20,7 +20,22 @@ public class MainActivity extends AppCompatActivity {
     View.OnClickListener btnShowImgListener = new View.OnClickListener() {
         @Override
         public void onClick(View v) {
-            dialogView = View.inflate(MainActivity.this, R.layout.dialog1, null)
+            dialogView = View.inflate(MainActivity.this, R.layout.dialog1, null);
         }
     };
+
+    View.OnClickListener btnShowListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            dialogView = View.inflate(MainActivity.this, R.layout.dialog, null);
+            dlgImg = dialogView.findViewById(R.id.img);
+            int selectedTitle = "";
+            switch (rg.getChecedRadioButtonId()){
+                case R.id.radio_dog:
+                    selectedId = R.drawable.dog;
+                    selectedTitle = radioDog.getText().toString();
+                    break;
+            }
+        }
+    }
 }
